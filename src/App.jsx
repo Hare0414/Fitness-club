be94d8866a84685e58b04a8c6635d3c4d200c635
jankojanici <janko.janicijevic@obinex.com>
2024-01-31 02:19:57 +0200

    implementation of Navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import './App.module.css'
+
+function App() {
+
+    return (
+        <div>
+            <Navbar />
+            {/* <Routes>
+                <Route exact path='/' element = {<Home />} />
+            </Routes> */}
+        </div>
+    )
+}
+export default App
