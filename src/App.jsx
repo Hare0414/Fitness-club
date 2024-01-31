23e7c2b37563c8bec5e96601ea692c41e11caa67
jankojanici <janko.janicijevic@obinex.com>
2024-01-31 10:47:26 +0200

    implement the gallery page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import {Route, Routes} from 'react-router-dom'
 import { Home } from './views/Home/Home.jsx'
+import { About } from './views/About/About.jsx'
+import { Gallery } from './views/Gallery/Gallery.jsx'
 import './App.module.css'
-import { formGroupClasses } from '@mui/material'
+
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
             <Navbar />
             <Routes>
                 <Route exact path='/' element = {<Home />} />
+                <Route exact path='/about' element ={ <About />} />
+                <Route exact path='/Gallery' element ={ <Gallery />} />
             </Routes>
         </div>
     )
