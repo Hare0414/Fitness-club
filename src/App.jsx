3c81cacb308208b1f6d1cd4cdef97d57afc07b8e
jankojanici <janko.janicijevic@obinex.com>
2024-01-31 04:04:30 +0200

    implement the mainhome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
+import {Route, Routes} from 'react-router-dom'
+import { Home } from './views/Home/Home.jsx'
 import './App.module.css'
+import { formGroupClasses } from '@mui/material'
 
 function App() {
 
     return (
         <div>
             <Navbar />
-            {/* <Routes>
+            <Routes>
                 <Route exact path='/' element = {<Home />} />
-            </Routes> */}
+            </Routes>
         </div>
     )
 }
---
+++ src/components/MainHome/MainHome.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './MainHome.module.css'
+import { NavLink } from 'react-router-dom'
+
+export const MainHome = () => {
+    return (
+        <div>
+            <section className={styles.home}>
+                <div className={styles.homeContent}>
+                    <h1>Train Different</h1>
+                    <h3>Body Fit & Perfect</h3>
+                    <div className={styles.btnBox}>
+                        <NavLink to="/classes">
+                            <span href="/classes">Our classes</span>
+                        </NavLink>
+                    </div>
+                </div>
+                <div className={styles.homeSci}>
+                    <a
+                        href="http://as-work.vercel.app"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <i className='bx bx-desktop'></i>
+                        </a>
+                    <a
+                        href="http://www.linkedin.com/in/serviusapolum"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <i className='bx bxl-linkedin' />
+                        </a>
+                    <a
+                        href="http://github.com/rhys0414"
+                        target="_blank"
+                        rel="noopener noreferrer">
+                            <i className='bx bxl-github' />
+                        </a>
+                </div>
+            </section>
+        </div>
+    )
+}
